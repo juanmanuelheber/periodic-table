b8df962825498d59bb4003ed5a2bc1093246cd88
juanmanuelheber <juan.m.heber@gmail.com>
2022-04-28 15:19:01 -0300

    Test share

--- src/components/common/PageTitle.js
+++ src/components/common/PageTitle.js
@@ -2,9 +2,22 @@ import React from 'react'
 import './PageTitle.scss'
 
 export const PageTitle = ({title=""}) => {
+    const hasNavigator = !!navigator.share
+    const share = () => {
+        if (hasNavigator) {
+            navigator.share({
+                text:"Table periodica de los elementos",
+                title: "Tabla Periódica",
+                url:"https://google.com.ar"
+            })
+        } else {
+            alert('No sharing support')
+        }
+    }
     return (
         <div className='container-title'>
             <h1 className='text-center p-3 p-md-3'>{title}</h1>
+            <button type="button" onClick={share}>Share</button>
         </div>
     )
 }
